164e0a8d0f1f3746a839d694455140a25051f922
SamsTek19 <sarfoagyei@gmail.com>
2025-12-26 22:28:04 +0000

    Update features and fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'project/src/*.html',
+          dest: '.'
+        },
+        {
+          src: 'project/src/*.css',
+          dest: '.'
+        },
+        {
+          src: 'project/src/*.js',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
